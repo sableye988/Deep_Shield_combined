25258c80f9f65957547261ed4d91dc99fa799618
sableye988 <sableye9@hs.ac.kr>
2025-09-29 10:13:53 +0900

    chore: add migrations folder for db schema management

---
+++ apps/mine/migrations/versions/dea78ea5f1c8_bootstrap_base.py
@@ -0,0 +1,24 @@
+"""bootstrap base
+
+Revision ID: dea78ea5f1c8
+Revises: 
+Create Date: 2025-09-29 02:03:54.176595
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dea78ea5f1c8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
